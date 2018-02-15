6de2399992d821f3f478a6913c86039968c656f5
Valentin Sponer <valentinsponer@Valentins-MacBook-Pro.local>
2018-02-15 09:47:35 -0800

    Initial commit of file1 and file2.

---
+++ file1.py
@@ -0,0 +1,39 @@
+
+
+
+x,y = float(input("Enter the value of x: ")),int(input("Enter the value of y: "))
+print("The value of x is",x)
+print("The value of y is",y)
+
+#arithmetic
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+print(x%y)
+
+x+=1
+y//=2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+print(x%y)
+
+x,y=y,x
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+print(x%y)
+
+#Questions for Brian Hill:
+#about the import statemants, why is import numble not working?
